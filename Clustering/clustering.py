1e67ed1b25a8ba7a069435ecb86820642e82f983
Marie LB <marielb@iPad-de-Marie-1.home>
2021-01-20 23:40:29 +0100

    Ajout brouillon.py et mise au propre du script - suite

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -2,10 +2,28 @@
 """
 Created on Tue Jan 19 22:30:47 2021
 
-@author: Le groupe tournesol
+@author: Le groupe tournesol (the best)
+
+A FAIRE :
+
+    - Finir les fonctions (duh)
+    - Se mettre d'accord sur un seuil minimal de plantes dans un rang
+    (==> nombre minimal de pixels à trouver.)
+    - Voir la robustesse des fonctions AutomaticNbClusters et
+    Threshold_PixelsRang dans types d'images
+    (dépend de la taille de l'image, du nombre total de rangs et le nombre
+    total de pixels "plantes" )
+    - Déterminer quelles variables nous allons utiliser pour vérifier la
+    qualité du dénombrement des plantes de l'image.
+
+
+    - Uniformiser les noms des variables (tout mettre en anglais)
+    - Commenter proprement en anglais
+    - Docstrings en anglais
+
 """
 
-# Libraries importation
+# Import of librairies
 from PIL import Image
 import numpy as np
 from sklearn.cluster import DBSCAN
@@ -13,7 +31,7 @@ import pandas as pd
 # pip install -U scikit-fuzzy
 import skfuzzy as fuzz
 
-# Image Opening
+# Open the binarized image
 path = ""  # path of the OTSU image
 img = Image.open('PATH')
 
@@ -62,6 +80,7 @@ def DBSCAN_clustering(img, eps_, min_samples_):
     mat_clustered = DBSCAN(eps=eps_, min_samples=min_samples_).fit(
         mat_coord
     )  # clustering using DBSCAN
+    # Panda dataframe
     dataframe_coord = pd.DataFrame(mat_coord)
     dataframe_coord = dataframe_coord.rename(columns={0: "X", 1: "Y"})
     label = pd.DataFrame(mat_clustered.labels_)
@@ -96,44 +115,77 @@ def PlantsDetection(dataframe_coord):
         of a multiple agents system. Its size is the number of rows and the
         size of a row is the number of plants (centroïd coordinates).
     """
+
     labels_rangs = np.unique(
         dataframe_coord[["label"]].to_numpy()
-    )  # Get labels from plants pixels
-    rangs_coord = []  # empty list of np array containing plants pixels from one rang
+    )  # Number of rows and their label
+    rangs_coord = []
+    # empty list of np.array containing plants pixels coordinates from one rang
     # rangs_clustered = []
-    fpcs = (
-        []
-    )
+    fpcs = []
     # Mesure de qualité du clustering, variable qui permet d'avoir une idée de
-    # la qualité du clustering , porhce de 1 c'est bien
+    # la qualité du clustering , comprise entre 0 et 1, bien quand proche de 1
+
+    # Paramètres de la fonction c-means à passer en paramètre par défaut ici
     e = 0.005  # Pas, pour la différence de partitionnement entre deux
     # itérations, a voir si on le met en parametre
     m_i = 2000  # Nombre maximal d'itération
     m_p = 2
     # Paramètre de flou, puissance appliquée à la matrice d'appartenance,
     # souvent autour de 2, pour le moment en dur
-    historique_clusters = [
-        [],
-        [],
-    ]  # Nombre de cluster initialement et finalement, permet de comparer la différence de cluster entre ce que l'on a mis et ce qui est finalement retourné.
-    JSON_final = []  # liste de liste (la première correspond à un rang et à l'intérieur chaque couple aux coordonnées du centroïde d'un cluster ie plante)
+
+    historique_clusters = [[], []]
+    # Nombre de cluster initialement et finalement, permet de comparer
+    # la différence de cluster entre ce que l'on a mis et ce qui est finalement
+    # retourné.
+
+    JSON_final = []
+    # liste de liste (la première correspond à un rang et à l'intérieur
+    # chaque couple aux coordonnées du centroïde d'un cluster ie plante)
+
     for i in labels_rangs:
         rang = dataframe_coord[
             dataframe_coord["label"] == i][['X', 'Y']].to_numpy().T
-        if Threshold_PixelsRang() is True:
-            nb_clusters = AutomaticNbCluster(rang)
-            resultat_fuzzyClustering, nbFinalClusters = Fuzzy_Clustering(rang, nb_clusters, e, m_p, m_i)
-            JSON_final.append([resultat_fuzzyClustering])
 
+        if Threshold_PixelsRang() is True:
+            nb_clusters = AutomaticNbClusters(rang)
+            historique_clusters[0].append(nb_clusters)
+            resultat_fuzzyClustering, nbFinalClusters = Fuzzy_Clustering(rang,
+                                                                         nb_clusters, e, m_p, m_i)
+            historique_clusters[1].append(nbFinalClusters)
+
+            JSON_final.append([resultat_fuzzyClustering])
 
     return JSON_final
 
-def Threshold_PixelsRang():
-    # Seuil (nb pixels) en dessous duquel, on considère que le résultat de DBSCAN ne soit pas un rang Ressort un bool
-    pass
+
+def Threshold_PixelsRang(rang, seuil):
+    """
+    Determine if there are enough pixels in the result of the DBSCAN_clustering
+    function. It can be considered as a row according to a threshold pixels.
+
+    Parameters
+    ----------
+    rang : List
+        List of 2 lists, the values of X and Y, respectively, coordinates of
+        the pixels representing the plants in the row.
+
+    Return
+    -------
+    isRow : Boolean
+        Boolean indicating if there are enough pixel in a row to count at least
+        a plant. A VERIFIER A PARTIR DE COMBIEN DE PLANTES DANS LE RANG ON
+        CONSIDERE OK POUR INITIALISATION D'UN AGENT PLANTE
+
+    """
+
+    if len(rang[0]) > seuil:
+        return True
+    else:
+        return False
 
 
-def AutomaticNbCluster(rang):
+def AutomaticNbClusters(rang):
     """
     Nombre de pixels dans chaque rangs en entrée
     (liste de X et Y, les coordonnées de chaque pixel du rang)
@@ -142,16 +194,19 @@ def AutomaticNbCluster(rang):
     Nombre de clusters dans le rang
 
     """
+
     nb_clusters = int(len(rang[0])/390)
-    if nb_clusters == 0:  # S'il y a trop peu de pixels
-       nb_clusters = 1
+
+    if nb_clusters == 0:  # If too few pixels, supplementary security
+        nb_clusters = 1
 
     return nb_clusters
 
 
 def Fuzzy_Clustering(rang, nb_clusters, e, m_p, m_i):
     """
-    En entrée, rang est la liste des coordonnées des pixels du rang (np.array eet 2 listes de longueur du nombre de pixels)
+    En entrée, rang est la liste des coordonnées des pixels du rang
+    (np.array et 2 listes de longueur du nombre de pixels)
 
     Fuzzy clustering pour un rang défini par DBSCAN (ici)
     Returns
@@ -161,15 +216,15 @@ def Fuzzy_Clustering(rang, nb_clusters, e, m_p, m_i):
     """
     cntr, u, u0, d, jm, p, fpc = fuzz.cmeans(rang, c=nb_clusters, m=m_p,
                                              error=e, maxiter=m_i)
-    # Attribution d'une étiquette à chaque point des données selon son cluster le plus probable (cf matrice u)
-
-    cluster = np.argmax(u, axis=0)  # Equivalent au label du DBSCAN
+    # Attribution d'une étiquette à chaque point des données selon son cluster
+    # le plus probable (cf matrice u)
 
     nbClusterFinal = len(u)
 
     PosCentCcluster = []
     for pt in cntr:
         PosCentCcluster.append([pt[0], pt[1]])
-# Ajouter d'autres paramètres pour vérifier la qualité du clustering comme fpc ou jm
-    return PosCentCcluster, nbClusterFinal
 
+    # Ajouter d'autres paramètres pour vérifier
+    # la qualité du clustering comme fpc ou jm
+    return PosCentCcluster, nbClusterFinal
