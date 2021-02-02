c649c5a879a4b1478affc673fee0c5fac2fe20b7
Marie LB <marielb@iPad-de-Marie-1.home>
2021-02-02 22:59:21 +0100

    Méthode scanner courbe médianne

--- Clustering/scanner.py
+++ Clustering/scanner.py
@@ -4,7 +4,102 @@ import pandas as pd
 from os import listdir
 from PIL import Image
 import matplotlib.pyplot as plt
+import statistics
 from scipy.optimize import curve_fit
+
+
+def Total_Plant_Position(path_image_input, epsilon, min_point):
+    list_image = listdir(path_image_input)
+    if '.DS_Store' in list_image:
+        list_image.remove('.DS_Store')
+    for image in list_image:
+        print("start ", image)
+        img = Image.open(path_image_input + "/" + image)
+        print("DBSCAN")
+        dataframe_coord = DBSCAN_clustering(img, epsilon, min_point)
+        coordPixelsMedian = pixel_median(dataframe_coord, img)
+        fitting = fit_curve(coordPixelsMedian)
+        plot_cluster(coordPixelsMedian, dataframe_coord, image, fitting)
+        # break
+    return
+
+
+def pixel_median(dataframe_coord, img):
+    # Get the image and transforms it into a np array
+    img_array = np.array(img)
+    # mat_coord = np.argwhere(img_array[:, :, 0] == 255)
+
+    # For each cluster determined by DBSCAN
+    label_row = np.unique(dataframe_coord[["label"]].to_numpy())
+    coord_Pixels_img = []
+
+    for row in label_row:
+        coord_row = dataframe_coord[dataframe_coord["label"] == row][[
+            "X", "Y"]].to_numpy()
+
+        coord_Pixels_row = [] # List of median pixels (one per line of the image)
+
+        # Get the median pixel for a row of pixels in a labelled row
+        # img_height = img_array.shape[0]
+        img_height = max(img_array.shape)
+
+        # Ici le pas est à 5 mais peut-être choisir un pas plus proportionnel à
+        # la taille de l'image (à faire empiriquement)
+        for i in range(0,img_height, 5):
+            interval_coord = np.where((coord_row > i) & (coord_row <= i+5))
+            if len(interval_coord) > 0:
+                # if len(np.where(np.logical_and(coord_row > i,
+                # coord_row < i+5))) > 0:
+                pos = interval_coord[0]
+                if len(coord_row[pos].T[0]) > 0:
+
+                    pixelMedian = statistics.median(coord_row[pos].T[0])
+                    #print(pixelMedian)
+                    coord_Pixels_row.append([int(pixelMedian),
+                                             coord_row[pos].T[1][0]])
+        coord_Pixels_img.append(coord_Pixels_row)
+
+    return coord_Pixels_img
+
+
+def pixel_minmax(dataframe_coord, img):
+    # Get the image and transforms it into a np array
+    img_array = np.array(img)
+    # mat_coord = np.argwhere(img_array[:, :, 0] == 255)
+
+    # For each cluster determined by DBSCAN
+    label_row = np.unique(dataframe_coord[["label"]].to_numpy())
+    coord_Pixels_imgMin = []
+    coord_Pixels_imgMax = []
+
+    for row in label_row:
+        coord_row = dataframe_coord[dataframe_coord["label"] == row][[
+            "X", "Y"]].to_numpy()
+
+        # List of min pixels (one per line of the image)
+        coord_Pixels_row_min = []
+        # List of max pixels
+        coord_Pixels_row_max = []
+
+        # Get the median pixel for a row of pixels in a labelled row
+        # img_height = img_array.shape[0]
+        img_height = max(img_array.shape)
+
+        for i in range(0,img_height, 5):
+            interval_coord = np.where((coord_row > i) & (coord_row <= i+5))
+                # if len(np.where(np.logical_and(coord_row > i,
+                # coord_row < i+5))) > 0:
+            pos = interval_coord[0]
+            if len(coord_row[pos].T[0]) > 0:
+
+                pixelMedian = statistics.median(coord_row[pos].T[0])
+                #print(pixelMedian)
+                coord_Pixels_row_min.append([int(pixelMedian),
+                                             coord_row[pos].T[1][0]])
+        coord_Pixels_imgMin.append(coord_Pixels_row_min)
+        coord_Pixels_imgMax.append(coord_Pixels_row_max)
+
+    return coord_Pixels_imgMin, coord_Pixels_imgMax
 
 
 def DBSCAN_clustering(img, epsilon, min_point):
@@ -27,37 +122,34 @@ def DBSCAN_clustering(img, epsilon, min_point):
     return dataframe_coord
 
 
-def Total_Plant_Position(path_image_input, epsilon, min_point):
-    list_image = listdir(path_image_input)
-    for image in list_image:
-        print("start ", image)
-        img = Image.open(path_image_input + "/" + image)
-        print("DBSCAN")
-        dataframe_coord = DBSCAN_clustering(img, epsilon, min_point)
-        fitting = find_equation(dataframe_coord)
-        plot_cluster(dataframe_coord, image, fitting)
-        break
-    return
-
-
 def func(x, a, b, c):
     y = a * (x ** 2) + b * x + c
     return y
 
 
-def find_equation(dataframe_coord):
-    label_row = np.unique(dataframe_coord[["label"]].to_numpy())
-    fit_label = {}
-    for row in label_row:
-        dataframe_coord_row = dataframe_coord[dataframe_coord["label"] == row][
-            ["X", "Y"]
-        ]
-        fitting, _ = curve_fit(func, dataframe_coord_row["X"], dataframe_coord_row["Y"])
-        fit_label.setdefault(row, fitting)
-    return fit_label
+def fit_curve(coordPixels):
+
+    for row in range(len(coordPixels)):
+        X = []
+        Y = []
+        row_label = []
+        for pixel in range(len(coordPixels[row])):
+            row_label.append(pixel)
+            X.append(coordPixels[row][pixel][0])
+            Y.append(coordPixels[row][pixel][1])
+        Xarray = np.array(X)
+        Yarray = np.array(Y)
+        row_array = np.array(row_label)
+
+        fitting, _ = curve_fit(func, Xarray, Yarray)
+
+        #fit_label.setdefault(row, fitting)
+
+    return fitting
 
 
-def plot_cluster(dataframe_coord, image, fit_label):
+def plot_cluster(coordPixels, dataframe_coord, image, fitting):
+
     fig = plt.figure(figsize=(8, 10))
     ax = fig.add_subplot(111)
     ax.scatter(
@@ -67,30 +159,85 @@ def plot_cluster(dataframe_coord, image, fit_label):
         s=0.5,
         cmap="Paired",
     )
-    label_row = np.unique(dataframe_coord[["label"]].to_numpy())
-    for row in label_row:
-        maxi = dataframe_coord[dataframe_coord["label"] == row]["X"].max()
-        mini = dataframe_coord[dataframe_coord["label"] == row]["X"].min()
+    for row in range(len(coordPixels)):
+        X = []
+        Y = []
+        for pixel in range(len(coordPixels[row])):
+            X.append(coordPixels[row][pixel][0])
+            Y.append(coordPixels[row][pixel][1])
 
-        ax.plot(
-            [x for x in range(mini, maxi, 1)],
-            [
-                func(
-                    x,
-                    fit_label.get(row)[0],
-                    fit_label.get(row)[1],
-                    fit_label.get(row)[2],
-                )
-                for x in range(mini, maxi, 1)
-            ],
-        )
+    ax.plot( X, Y, '+',c='r')
+    fitting_int = [int(i) for i in fitting]
+    print(fitting_int)
+    ax.plot(X, func(np.array(X), *fitting_int), 'b-')
+
+
+        # ax.plot(
+        #     [x for x in range(mini, maxi, 1)],
+        #     [
+        #         func(
+        #             x,
+        #             fit_label.get(row)[0],
+        #             fit_label.get(row)[1],
+        #             fit_label.get(row)[2],
+        #         )
+        #         for x in range(mini, maxi, 1)
+        #     ],
+        # )
+
     plt.show()
-    fig.savefig("/home/fort/Bureau/results/" + image.split(".")[0] + ".png")
+    #fig.savefig("/home/fort/Bureau/results/" + image.split(".")[0] + ".png")
     return
 
 
+# def find_equation(dataframe_coord):
+#     label_row = np.unique(dataframe_coord[["label"]].to_numpy())
+#     fit_label = {}
+#     for row in label_row:
+#         print("ROW ", row, type(row))
+#         dataframe_coord_row = dataframe_coord[dataframe_coord["label"] == row][
+#             ["X", "Y"]
+#         ]
+#         print("coordonnees rang", dataframe_coord_row)
+#         fitting, _ = curve_fit(func, dataframe_coord_row["X"], dataframe_coord_row["Y"])
+#         fit_label.setdefault(row, fitting)
+#     return fit_label
+
+
+# def plot_cluster(dataframe_coord, image, fit_label):
+#     fig = plt.figure(figsize=(8, 10))
+#     ax = fig.add_subplot(111)
+#     ax.scatter(
+#         dataframe_coord["X"].tolist(),
+#         dataframe_coord["Y"].tolist(),
+#         c=dataframe_coord["label"].tolist(),
+#         s=0.5,
+#         cmap="Paired",
+#     )
+#     label_row = np.unique(dataframe_coord[["label"]].to_numpy())
+#     for row in label_row:
+#         maxi = dataframe_coord[dataframe_coord["label"] == row]["X"].max()
+#         mini = dataframe_coord[dataframe_coord["label"] == row]["X"].min()
+
+#         ax.plot(
+#             [x for x in range(mini, maxi, 1)],
+#             [
+#                 func(
+#                     x,
+#                     fit_label.get(row)[0],
+#                     fit_label.get(row)[1],
+#                     fit_label.get(row)[2],
+#                 )
+#                 for x in range(mini, maxi, 1)
+#             ],
+#         )
+#     plt.show()
+#     #fig.savefig("/home/fort/Bureau/results/" + image.split(".")[0] + ".png")
+#     return
+
+
 Total_Plant_Position(
-    path_image_input="/home/fort/Documents/APT 3A/Cours/Ekinocs/Output_General/Output/Session_2/Otsu_R",
-    epsilon=20,
+    path_image_input="./../../images",
+    epsilon=7,
     min_point=10,
 )
