c0291cb965877e2bcebb9080c92b2966c2ad5a81
lamauvaiseherbe <hugo.vaysset@polytechnique.edu>
2021-02-16 17:14:21 +0100

    launched on 02_07

--- Whole_Process/WholeProcess.py
+++ Whole_Process/WholeProcess.py
@@ -40,26 +40,26 @@ def CompleteProcess(_path_input_rgb_img, _path_output_root,
                     _RAs_group_size=20, _RAs_group_steps=2, _Simulation_steps=50,
                     _RALs_fuse_factor=0.5, _RALs_fill_factor=1.5, densite=None, experiment=None):
     
-    # PiFT.All_Pre_Treatment(_path_input_rgb_img,
-    #                   _path_output_root,
-    #                   _path_position_files,
-    #                   _rows_real_angle,
-    #                   _make_unique_folder_per_session, _session,
-    #                   _do_Otsu, _do_AD,
-    #                   _save_AD_score_images, _save_BSAS_images,
-    #                   _bsas_threshold)
+    PiFT.All_Pre_Treatment(_path_input_rgb_img,
+                      _path_output_root,
+                      _path_position_files,
+                      _rows_real_angle,
+                      _make_unique_folder_per_session, _session,
+                      _do_Otsu, _do_AD,
+                      _save_AD_score_images, _save_BSAS_images,
+                      _bsas_threshold)
     
-    # FA.All_Fourier_Analysis(_path_output_root,
-    #                      _session,
-    #                      _bin_div_X, _bin_div_Y)
+    FA.All_Fourier_Analysis(_path_output_root,
+                         _session,
+                         _bin_div_X, _bin_div_Y)
     
-    MIS.All_Simulations(_path_input_rgb_img,
-                    _path_output_root,
-                    _labelled_images,
-                    _session,
-                    _RAs_group_size, _RAs_group_steps, _Simulation_steps,
-                    _RALs_fuse_factor, _RALs_fill_factor,
-                    densite=densite, experiment=experiment)
+    # MIS.All_Simulations(_path_input_rgb_img,
+    #                 _path_output_root,
+    #                 _labelled_images,
+    #                 _session,
+    #                 _RAs_group_size, _RAs_group_steps, _Simulation_steps,
+    #                 _RALs_fuse_factor, _RALs_fill_factor,
+    #                 densite=densite, experiment=experiment)
 
 if (__name__=="__main__"):
     # root = "D:/Documents/IODAA/Fil Rouge/Resultats/dIP_vs_dIR_linear"
@@ -110,15 +110,15 @@ if (__name__=="__main__"):
     #                 log_file.write("\n")
     #                 log_file.write("AN ERROR OCCURED")
 
-    CompleteProcess(_path_input_rgb_img="D:/Documents/IODAA/Fil Rouge/Resultats/dIP_vs_dIR_curved/densite=7/027_054/virtual_reality",
-                    _path_output_root="D:/Documents/IODAA/Fil Rouge/Resultats/dIP_vs_dIR_curved/densite=7/027_054_analysis/",
+    CompleteProcess(_path_input_rgb_img="D:/Documents/IODAA/Fil Rouge/Resultats/dIP_vs_dIR_curved_more_ratio/densite=7/02_07/virtual_reality",
+                    _path_output_root="D:/Documents/IODAA/Fil Rouge/Resultats/dIP_vs_dIR_curved_more_ratio/densite=7/02_07_analysis/",
     # CompleteProcess(_path_input_rgb_img="D:/Documents/IODAA/Fil Rouge/Resultats/dIP_vs_dIR_linear_fixed_density/densite=5/0.22_0.88/virtual_reality",
     #                 _path_output_root="D:/Documents/IODAA/Fil Rouge/Resultats/dIP_vs_dIR_linear_fixed_density_analysis/densite=5/0.22_0.88/",
                     
     # Trois lignes sur ma branche (pas a jour) et pas presentes sur la ligne de Baptiste
                      _labelled_images = True,
-                     _path_position_files="D:/Documents/IODAA/Fil Rouge/Resultats/dIP_vs_dIR_curved/densite=7/027_054/Position_Files",
-                     _rows_real_angle=90,
+                     _path_position_files="D:/Documents/IODAA/Fil Rouge/Resultats/dIP_vs_dIR_curved_more_ratio/densite=7/02_07/Position_Files",
+                     _rows_real_angle=0,
 
                     _make_unique_folder_per_session=False, _session=1,
                     _do_Otsu=True, _do_AD=True,
